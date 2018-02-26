088ad6a977b57fc7da0de03ddd487ded3bfeecc4
liaronce <qq1062787843@hotmail.com>
2018-02-26 20:51:58 +0800

    Update to ES6

--- src/gml/completionProvider.js
+++ src/gml/completionProvider.js
@@ -12,26 +12,26 @@ class GMLCompletionItemProvider {
         if (!enable) {
             return Promise.resolve(result);
         }
-        var range = document.getWordRangeAtPosition(position);
-        var prefix = range ? document.getText(range) : '';
+        let range = document.getWordRangeAtPosition(position);
+        const prefix = range ? document.getText(range) : '';
         if (!range) {
             range = new vscode_1.Range(position, position);
         }
-        var added = {};
-        var createNewProposal = function (kind, name, entry, type) {
-            var proposal = new vscode_1.CompletionItem(name);
+        const added = {};
+        const createNewProposal = (kind, name, entry, type) => {
+            const proposal = new vscode_1.CompletionItem(name);
             proposal.kind = kind;
             if (entry) {
                 if (entry.description) {
                     proposal.documentation = entry.description;
                 }
                 if (entry.parameters) {
-                    let signature = type ? '(' + type + ') ' : '';
+                    let signature = type ? `(${type}) ` : '';
                     signature += name;
                     signature += '(';
                     if (entry.parameters && entry.parameters.length != 0) {
                         let params = '';
-                        entry.parameters.forEach(p => params += p.label + ',');
+                        entry.parameters.forEach(p => params += `${p.label},`);
                         signature += params.slice(0, -1);
                     }
                     signature += ')';
@@ -40,38 +40,38 @@ class GMLCompletionItemProvider {
             }
             return proposal;
         };
-        var matches = (name) => {
+        const matches = (name) => {
             return prefix.length === 0 || name.length >= prefix.length && name.substr(0, prefix.length) === prefix;
         };
-        for (var globalvariables in gmlGlobals.globalvariables) {
+        for (const globalvariables in gmlGlobals.globalvariables) {
             if (gmlGlobals.globalvariables.hasOwnProperty(globalvariables) && matches(globalvariables)) {
                 added[globalvariables] = true;
                 result.push(createNewProposal(vscode_1.CompletionItemKind.Variable, globalvariables, gmlGlobals.globalvariables[globalvariables]));
             }
         }
-        for (var globalfunctions in gmlGlobals.globalfunctions) {
+        for (const globalfunctions in gmlGlobals.globalfunctions) {
             if (gmlGlobals.globalfunctions.hasOwnProperty(globalfunctions) && matches(globalfunctions)) {
                 added[globalfunctions] = true;
                 result.push(createNewProposal(vscode_1.CompletionItemKind.Function, globalfunctions, gmlGlobals.globalfunctions[globalfunctions]));
             }
         }
-        for (var constants in gmlGlobals.constants) {
+        for (const constants in gmlGlobals.constants) {
             if (gmlGlobals.constants.hasOwnProperty(constants) && matches(constants)) {
                 added[constants] = true;
                 result.push(createNewProposal(vscode_1.CompletionItemKind.Field, constants, gmlGlobals.constants[constants]));
             }
         }
-        for (var keywords in gmlGlobals.keywords) {
+        for (const keywords in gmlGlobals.keywords) {
             if (gmlGlobals.keywords.hasOwnProperty(keywords) && matches(keywords)) {
                 added[keywords] = true;
                 result.push(createNewProposal(vscode_1.CompletionItemKind.Keyword, keywords, gmlGlobals.keywords[keywords]));
             }
         }
-        var text = document.getText();
-        var functionMatch = /^\w+\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\s*\(/mg;
-        var match = null;
+        const text = document.getText();
+        const functionMatch = /^\w+\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\s*\(/mg;
+        let match = null;
         while (match = functionMatch.exec(text)) {
-            var word = match[1];
+            const word = match[1];
             if (!added[word]) {
                 added[word] = true;
                 result.push(createNewProposal(vscode_1.CompletionItemKind.Function, word, null));
--- src/gml/hoverProvider.js
+++ src/gml/hoverProvider.js
@@ -1,23 +1,24 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-var vscode_1 = require("vscode");
-var gmlGlobals = require("./gmlGlobals");
-var GMLHoverProvider = (function () {
-    function GMLHoverProvider() {
+const vscode_1 = require("vscode");
+const gmlGlobals = require("./gmlGlobals");
+const GMLHoverProvider = ((() => {
+    class GMLHoverProvider {
+        provideHover(document, position, _token) {
+            let enable = vscode_1.workspace.getConfiguration('gml-gm81' || 'gml-gms' || 'gml-gms2').get('suggest.basic', true);
+            if (!enable) {
+                return undefined;
+            }
+            const wordRange = document.getWordRangeAtPosition(position);
+            if (!wordRange) {
+                return undefined;
+            }
+            const name = document.getText(wordRange);
+            const entry = gmlGlobals.globalfunctions[name] || gmlGlobals.constants[name] || gmlGlobals.globalvariables[name] || gmlGlobals.keywords[name];
+            return undefined;
+        }
     }
-    GMLHoverProvider.prototype.provideHover = function (document, position, _token) {
-        let enable = vscode_1.workspace.getConfiguration('gml-gm81' || 'gml-gms' || 'gml-gms2').get('suggest.basic', true);
-        if (!enable) {
-            return undefined;
-        }
-        var wordRange = document.getWordRangeAtPosition(position);
-        if (!wordRange) {
-            return undefined;
-        }
-        var name = document.getText(wordRange);
-        var entry = gmlGlobals.globalfunctions[name] || gmlGlobals.constants[name] || gmlGlobals.globalvariables[name] || gmlGlobals.keywords[name];
-        return undefined;
-    };
+
     return GMLHoverProvider;
-}());
+})());
 exports.default = GMLHoverProvider;
