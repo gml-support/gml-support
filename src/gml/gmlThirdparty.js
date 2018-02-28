cead5ea97dc2ca8317cf20ae21be81cf0ae9f10b
liaronce <qq1062787843@hotmail.com>
2018-02-28 12:06:58 +0800

    Add Third-party functions.

--- src/gml/gmlThirdparty.js
+++ src/gml/gmlThirdparty.js
@@ -2,8 +2,40 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 
 exports.thirdfunctions = {
-    liaronce_test: {
-        description: '[Function] (LiarOnce Function Test)',
-        signature: '(test, value)'
-    }
+    //Plugin: MaizeMusic
+    //Author: Mage 松仁玉米
+    //Website: http://www.magecorn.com
+    mm_init: {
+        description: '[Function] (MaizeMusic Plugin)',
+        signature: '(path)'
+    },
+
+    //Plugin: FoxWriting
+    //Author: NoisyFox
+    //Website: https://noisyfox.io
+    fw_add_font: {
+        description: '[Function] (FoxWriting Plugin)',
+        signature: '(name, size, bold, italic, stroke)'
+    },
+
+    //Plugin: FoxWinShave
+    //Author: NoisyFox
+    //Website: https://noisyfox.io
+    NF_WS_Init: {
+        description: '[Function] (FoxWinShave Plugin)'
+    },
+    NF_WS_ShaveWindow: {
+        description: '[Function] (FoxWinShave Plugin)',
+        signature: '(sprite, subimage, x, y)'
+    },
+    NF_WS_ShaveWindowAlpha: {
+        description: '[Function] (FoxWinShave Plugin)',
+        signature: '(sprite, subimage, x, y, alpha)'
+    },
+    NF_WS_ResetWindow: {
+        description: '[Function] (FoxWinShave Plugin)'
+    },
+    NF_WS_Free: {
+        description: '[Function] (FoxWinShave Plugin)'
+    },
 };
