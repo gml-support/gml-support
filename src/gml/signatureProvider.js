81106eeda39d94666fd9af1735701a1d60ad55d3
LiarOnce <qq1062787843@hotmail.com>
2019-11-07 09:32:47 +0800

    Fix Auto Closing Pairs for comment

--- src/gml/signatureProvider.js
+++ src/gml/signatureProvider.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode_1 = require("vscode");
 const gmlGlobals = require("./gmlGlobals");
-const documentationImporter = require("./documentationImporter");
 const _NL = '\n'.charCodeAt(0);
 const _TAB = '\t'.charCodeAt(0);
 const _WSB = ' '.charCodeAt(0);
